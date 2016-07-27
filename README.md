ebdd9473bc80e4e57fd49bb5071eb9166a69b193
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2016-07-27 03:41:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DotArchaius
+
+**DotArchaius** is a .NET port of [Netflix](https://github.com/Netflix) Java/Scala configuration management library [Archaius](https://github.com/Netflix/archaius).
---
+++ src/DotArchaius/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("DotArchaius")]
+[assembly: AssemblyProduct("DotArchaius")]
+[assembly: AssemblyDescription(".NET port of Netflix configuration management library Archaius")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 DotArchaius")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
